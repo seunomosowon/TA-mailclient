f371eec1d8c7478d3d0e29fc733d0bf3f1bd66c9
Seun Omosowon <seunomosowon@gmail.com>
2020-12-06 20:31:09 +0000

    Remove support for insecure connections
    Remove support for insecure connections and upgrade to version number 1.40

--- bin/mail_utils.py
+++ bin/mail_utils.py
@@ -7,22 +7,20 @@ import os
 import socket
 
 
-def mail_connectivity_test(server, protocol, is_secure):
+def mail_connectivity_test(server, protocol):
     """
     This validates connectivity to given hostname and port
     :param server: This is the remote hostname or IP to be used for the test.
     :type server: basestring
-    :param protocol: The protocol to be used to fetch emails - IMAP or POP3
+    :param protocol: The protocol to be used to fetch emails - IMAPS or POP3S
     :type protocol: basestring
-    :param is_secure: true/false
-    :type is_secure: bool
     :return: Raises an exception back to the modinput validation if connectivity test fails
     """
     try:
         captive_dns_addr = socket.gethostbyname(server)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
-        s.connect((captive_dns_addr, get_mail_port(protocol=protocol, is_secure=is_secure)))
+        s.connect((captive_dns_addr, get_mail_port(protocol=protocol)))
         s.close()
     except socket.error as e:
         raise socket.error("Socket error : %s" % e)
@@ -81,28 +79,18 @@ def bool_variable(x):
     return x
 
 
-def get_mail_port(protocol, is_secure):
+def get_mail_port(protocol):
     """
     This returns the server port to use for POP retrieval of mails
     :param protocol: The protocol to be used to fetch emails - IMAP or POP3
     :type protocol: basestring
-    :param is_secure: Determines if the mails should be fetched over SSL
-    :type is_secure: bool
     :return: Returns the correct port for either POP3 or POP3 over SSL
     :rtype: int
     """
-    if is_secure:
-        if protocol == 'POP3':
-            port = 995
-        elif 'IMAP' == protocol:
-            port = 993
-        else:
-            raise Exception("Invalid options passed to get_mail_port")
+    if protocol == 'POP3':
+        port = 995
+    elif 'IMAP' == protocol:
+        port = 993
     else:
-        if protocol == 'POP3':
-            port = 110
-        elif protocol == 'IMAP':
-            port = 143
-        else:
-            raise Exception("Invalid options passed to get_mail_port")
+        raise Exception("Invalid options passed to get_mail_port")
     return port
